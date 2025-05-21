f981897a7af29323968d8abdd89470269b44711e
Annylory LR <annyloryrosa@gmail.com>
2025-05-21 15:32:10 -0300

    did  factory and test for "post /events/"

--- tests/events.test.ts
+++ tests/events.test.ts
@@ -1,7 +1,7 @@
 import supertest from "supertest";
 import app from "../src/app";
 import prisma from "../src/database";
-import { createEvents } from "./factories/events-factory";
+import { createEvents, generateEventBody } from "./factories/events-factory";
 import { getEventId } from "./factories/tickets-factory";
 
 const api = supertest(app);
@@ -48,5 +48,22 @@ describe("GET /events/:id", () => {
                     date:expect.any(String)
                 })
         )   
+    })
+})
+
+describe("POST /events", () => {
+    it("should create a contact and return the status 201", async () => {
+        const data = generateEventBody();
+
+        const { status } = await api.post("/events").send(data);
+
+        expect(status).toBe(201);
+
+        const existentEvent = await prisma.event.findUnique({
+            where: { name: data.name }
+        });
+
+        expect(existentEvent).not.toBeNull();
+
     })
 })
--- tests/factories/events-factory.ts
+++ tests/factories/events-factory.ts
@@ -1,12 +1,19 @@
 import prisma from "../../src/database";
 import { faker } from "@faker-js/faker";
 
+export function generateEventBody(){
+    return{
+        name: faker.company.name(),
+        date: faker.date.future()
+    }
+}
 
 function generateEvents(numberOfEvents:number){
     const events = [];
 
     for(let i=0; i < numberOfEvents; i++){
-        events.push({name:faker.company.name(), date:faker.date.future()});
+        events.push({name:faker.company.name(),
+                     date:faker.date.future()});
     }
 
     return events;
