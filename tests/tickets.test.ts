5db0a921ae36df84645fa2e5688992d4696d7447
Annylory LR <annyloryrosa@gmail.com>
2025-05-21 18:50:51 -0300

    finished happy tests for tickets

--- tests/tickets.test.ts
+++ tests/tickets.test.ts
@@ -1,6 +1,7 @@
 import supertest from "supertest";
 import app from "../src/app";
-import prisma from  "../src/database"
+import prisma from  "../src/database";
+import { faker } from "@faker-js/faker";
 import { createPurchases, getEventId } from "./factories/tickets-factory";
 import { createEvents } from "./factories/events-factory";
 
@@ -32,5 +33,59 @@ describe("GET /tickets/:eventId", () => {
                 })
             ])
         )
+    })
+})
+
+describe("POST /tickets", () => {
+    it("should create a ticket purchase and return status 201", async () => {
+        await createEvents(3);
+
+        const eventId = await getEventId();
+
+        const data = {
+            code:faker.string.alphanumeric(5),
+            owner:faker.person.firstName(),
+            eventId
+        }
+
+        const { status, body } = await api.post("/tickets").send(data);
+     
+        expect(status).toBe(201);
+
+        const existentTicket = await prisma.ticket.findUnique({
+            where: { id : body.id}
+        })
+
+        expect(existentTicket).not.toBeNull();
+
+    })
+})
+
+describe("PUT /tickets/use/:id", () => {
+    it("should update a ticket purchase to used and return status 204", async () => {
+        await createEvents(3);
+
+        const eventId = await getEventId();
+
+        const data = await prisma.ticket.create({
+            data:{
+                code:faker.string.alphanumeric(5),
+                owner:faker.person.firstName(),
+                eventId
+           }
+        })
+
+        const id = data.id
+       
+        const { status } = await api.put(`/tickets/use/${id}`);
+        
+        expect(status).toBe(204);
+
+        const updated = await prisma.ticket.findUnique({
+            where: { id: id }
+        })
+
+        expect(updated).not.toBeNull();
+
     })
 })
